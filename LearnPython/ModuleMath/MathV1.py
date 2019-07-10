e737df03e3a4598abbc061899a9d8fee43b875ad
monkiravn <monkiradgbn01@gmail.com>
2019-07-10 22:21:18 +0700

    First commit

---
+++ LearnPython/ModuleMath/MathV1.py
@@ -0,0 +1,10 @@
+import math
+
+#print(math.ceil(5.2)) #Lam tron len
+#print(math.copysign(1.0,2.0)) #Tra ve gia tri tuyet doi cua x nhung dau cua y
+#print(math.fabs(-2.5)) #Tra ve gia tri tuyet doi
+#print(math.factorial(3)) # Tra ve gia tri gia thua vd 3!
+#print(math.floor(5.2)) #Lam tron xuong
+#print(math.fmod(5.0,2.0)) #Chia lay phan du
+#print(math.frexp(9)) # Tra ve (m , n) voi x = m*2**n
+#print(math.gcd(36,9)) # Tra ve UCLN
