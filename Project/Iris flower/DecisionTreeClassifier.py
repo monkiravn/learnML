22aa2edcfd7b8c25bdd5bad2a7451fb6ee419cb3
monkiravn <monkiradgbn01@gmail.com>
2019-07-14 18:12:11 +0700

    Decisition Tree Classifier

--- Project/Iris flower/DecisionTreeClassifier.py
+++ Project/Iris flower/DecisionTreeClassifier.py
@@ -2,14 +2,25 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 import numpy as np
-np.random.seed(7)
-iris = datasets.load_iris()
 
+np.random.seed(7)
+
+iris = datasets.load_iris()
 X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size= 130)
 print(len(X_test))
+
 model = DecisionTreeClassifier()
 model.fit(X_train,y_train)
+
+def irisClassfication(irisPredic):
+    if(irisPredic == 2):
+        return "I. virginica"
+    elif(irisPredic == 1):
+        return "I. versicolor"
+    else:
+        return "I. setosa"
+
 X_New = np.array([[6.4, 3.1, 5.0, 2.0]])
 result = model.predict(X_New)
-print(result)
+print("Iris's type:",irisClassfication(result))
 print("Accuracy of model : %.2f %%" %(100*model.score(X_test, y_test)))
