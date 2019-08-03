5a46ef4dfc5b8778e7532b0f48de39a00911e7c0
monkiravn <monkiradgbn01@gmail.com>
2019-08-03 13:50:42 +0700

    update

---
+++ LearnPython/HomeWorkW2 AIVN/ex4-ELU.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+alpha = 0.1
+def ELU(data):
+    result = []
+    N = len(data)
+    for i in range(N):
+        if (data[i] < 0):
+            temp = alpha*(np.e**data[i] - 1) 
+            result.append(temp)
+        else:
+            temp = data[i]
+            result.append(temp)
+    return result
+
+data = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10]
+result = ELU(data)
+plt.plot(data,result)
+plt.xlabel('x values')
+plt.ylabel('ELU values')
+plt.title('ELU Function')
+plt.show()
---
+++ LearnPython/HomeWorkW2 AIVN/ex5-assignment.py
@@ -0,0 +1,53 @@
+import numpy as np 
+import random
+from collections import Counter
+
+data = [1,2,3,4,5,6,7,8,9]
+sum = np.sum(data)
+new_data = [1/sum , 2 /sum,3/sum,4/sum,5/sum,6/sum,7/sum,8/sum,9/sum]
+new_data[-1] = 1 - new_data[-1]
+for i in range(len(new_data) - 1):
+    new_data[-i-2] = new_data[-i-1] - new_data[-i-2]
+
+def point(number):
+#    if ( number > 0 and number < 1/sum):
+#        return 0
+    if ( number >= new_data[0] and number < new_data[1]):
+        return 1
+    elif ( number >= new_data[1] and number < new_data[2]):
+        return 2
+    elif ( number >= new_data[2] and number < new_data[3]):
+        return 3
+    elif ( number >= new_data[3] and number < new_data[4]):
+        return 4
+    elif ( number >= new_data[4] and number < new_data[5]):
+        return 5
+    elif ( number >= new_data[5] and number < new_data[6]):
+        return 6
+    elif ( number >= new_data[6] and number < new_data[7]):
+        return 7
+    elif ( number >= new_data[7] and number < new_data[8]):
+        return 8
+    else: 
+        return 9
+
+def rand(iters):
+    result = []
+    temp = 0
+    for i in range(iters):
+        number = random.random()
+        temp = point(number)
+        result.append(temp)
+    return result
+
+pointP = rand(1000)
+point_C = Counter(pointP)
+point_fren = point_C.most_common()
+point_fren.sort()
+fren = 0
+for p in point_fren:
+    fren += p[1]
+    print('{0}\t{1}'.format(p[0],p[1]))
+print(fren)
+
+
